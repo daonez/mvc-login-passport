f4051fcae79922ec0efdb5926fb00089ddf5bf46
daonez <cyongchoi@gmail.com>
2022-04-06 18:53:37 +0900

    초기세팅완료

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 //js
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT || 4000
-app.listen(PORT, console.log("Server don start for port: " + PORT))
+const loginRouter = require("./routes/login")
+
+app.set("view engine", "ejs")
+//Routes
+app.use("/", loginRouter)
+app.get("/", (req, res) => {
+  res.send("hello")
+})
+const PORT = 4000
+app.listen(PORT, console.log("Server has started at port " + PORT))
