4a6629158276152d5bfa803d51885350148d6e43
daonez <cyongchoi@gmail.com>
2022-04-08 15:33:19 +0900

    wip

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 const express = require("express")
 const app = express()
 const loginRouter = require("./routes/login")
-
+const passport = require("passport")
+const { loginCheck } = require("./auth/passport")
 const connect = require("./models")
+const session = require("express-session")
 connect()
+loginCheck(passport)
 
 app.set("view engine", "ejs")
 //Routes
@@ -14,3 +17,21 @@ app.get("/", (req, res) => {
 })
 const PORT = 4000
 app.listen(PORT, console.log("Server has started at port " + PORT))
+
+
+app.set("view engine", "ejs")
+
+//BodyParsing
+app.use(express.urlencoded({ extended: false }))
+app.use(
+  session({
+    secret: "oneboy",
+    saveUninitialized: true,
+    resave: true,
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+//Routes
+app.use("/", require("./routes/login"))
